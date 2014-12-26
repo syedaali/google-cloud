55035330a66d1b28114b6e6f9fac34cf350e3148
Syed Ali <syed_a_ali@yahoo.com>
2014-12-25 18:38:39 -0800

    added comments

--- gae/hello/hello.py
+++ gae/hello/hello.py
@@ -1,5 +1,15 @@
 __author__ = 'syedaali'
 
+'''
+This program is a demonstration of Google App Engine.
+It uses webapp2 framework to print "Hello Google!'.
+
+How to use this program:
+  Download the Google Python SDK, including the dev app server
+  Run 'dev_appserver.py hello'. Hello should be the directory that contains hello.py and app.yaml
+  Following that in you browser visit http://localhost:8080
+'''
+
 import webapp2
 
 MAIN_PAGE = 'Hello Google!'
